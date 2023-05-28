62b7da4f2ed6a69873e387fa1886c858a57f8d4f
ReinisTams <125902838+ReinisTams@users.noreply.github.com>
2023-05-29 01:17:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,36 @@
 
 class Query:
     def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
+        self.type = query[0]  #Piešķir pirmo elementu
+        self.number = int(query[1])  #Pārvērš otro elementu no vaicājuma par veselu skaitli un piešķir number"
         if self.type == 'add':
-            self.name = query[2]
-    
-    def phone_book_manager(N, queries):
-    phone_book = {}
+            self.name = query[2]  #Piešķir trešo elementu no vaicājuma atribūtam "name", ja ir "add"
 
-    for query in queries:
-      query_parts = query.split()
-    
-      if query_parts[0] == 'add':
-         phone_number = query_parts[1]
-         name = query_parts[2]
-         phone_book[phone_number] = name
-   elif query_parts[0] == 'del':
-         phone_number = query_parts[1]
-          if phone_number in phone_book:
-          del phone_book[phone_number]
-        
-   elif query_parts[0] == 'find':
-     phone_number = query_parts[1]
-       if phone_number in phone_book:
-          print(phone_book[phone_number])
-       else:
-          print('not found')
+def phone_book_manager(N, queries):
+    phone_book = {}  # Izveido tukšu vārdnīcu, kas būs kā telefona grāmata
 
-N = int(input())
-queries = [input() for _ in range(N)]
-phone_book_manager(N, queries)
+    for query in queries:  #ciklē cauri katram vaicājumam
+        query_parts = query.split()  #Sadala vaicājumu atsevišķos elementos pēc atstarpēm
+
+        if query_parts[0] == 'add':  #Ja vaicājuma pirmā daļa ir "add"
+            phone_number = query_parts[1]  #Piešķir otru daļu atribūtam "phone_number"
+            name = query_parts[2]  #Piešķir trešo daļu atribūtam "name"
+            phone_book[phone_number] = name  #Pievieno telefona numuru un vārdu telefona grāmatas vārdnīcai
+
+        elif query_parts[0] == 'del':  #Vaicājuma pirmā daļa ir del
+            phone_number = query_parts[1]  #Piešķir otru daļu 'phone_number'
+            if phone_number in phone_book:  #Pārbauda vai telefona numurs ir telefona grāmatā
+                del phone_book[phone_number]  #Izdzēš telefona numuru un saistīto vārdu no telefona grāmatas
+
+        elif query_parts[0] == 'find':  #ja vaicājuma pirmā daļa ir "find"
+            phone_number = query_parts[1]  #Piešķir otru daļu "phone_number"
+            if phone_number in phone_book:  #Pārbauda vai telefona numurs ir telefona grāmatā
+                print(phone_book[phone_number])  #Izdrukā vārdu, kas saistīts ar telefona numuru
+            else:
+                print('not found')  #Drukā "not found", ja telefona numurs nav telefona grāmatā
+
+N = int(input())  #Lasīt N vērtību no ievades
+queries = [input() for _ in range(N)]  #Lasīt vaicājumus no ievades
+
+phone_book_manager(N, queries)  #Saukt phone_book_manager funkciju ar ievades vērtībām
 
